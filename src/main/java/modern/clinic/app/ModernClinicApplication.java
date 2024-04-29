1a9f5574195f9f99d697291eb70f07befaa29b1a
karol-dziachan <karol.dziachan@gmail.com>
2024-04-29 17:02:30 +0200

    Init Java

---
+++ src/main/java/modern/clinic/app/ModernClinicApplication.java
@@ -0,0 +1,13 @@
+package modern.clinic.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/modern/clinic/app/ModernClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package modern.clinic.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
